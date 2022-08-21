25f9cdb8b4cfa2465efe495d95640b03b46cf950
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-21 16:15:38 +0300

    HW 5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        int g = (1000 * 7);
-        int days1 = (g / 250);
-        int days2 = (g / 500);
-        int mean = ((days1 + days2) / 2);
-        System.out.println("Теряя 250г в день: " + days1 + " дней");
-        System.out.println("Теряя 500г в день: " + days2 + " дней");
-        System.out.println("В среднем " + mean + " дней");
+        int masha = 67760;
+        int uptom = (masha / 100 * 10);
+        int summ = (masha + uptom);
+        int yearm = (summ * 12);
+        int diffm = (yearm - masha * 12);
+        System.out.println("Маша теперь получает: " + summ + "р" + "\n"
+        + "Годовой доход вырос на " + diffm + "р");
+        int denis = 83690;
+        int uptod = (denis / 100 * 10);
+        int sumd = (denis + uptod);
+        int yeard = (sumd * 12);
+        int diffd = (yeard - denis * 12);
+        System.out.println("Денис теперь получает: " + sumd + "р" + "\n"
+        + "Годовой доход вырос на " + diffd + "р");
+        int kristina = 76230;
+        int uptok = (kristina / 100 * 10);
+        int sumk = (kristina + uptok);
+        int yeark = (sumk *12);
+        int diffk = (yeark - kristina * 12);
+        System.out.println("Кристина теперь получает: " + sumk + "р" + "\n"
+        + "Годовой доход вырос на " + diffk + "р");
+
     }
 }
