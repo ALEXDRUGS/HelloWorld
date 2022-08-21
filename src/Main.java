785a2913630acfc2c1d04f648560433007705f4c
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-08-21 15:05:17 +0300

    HW 4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       int bananas = (5 * 80);
-       int milk = (200 / 100 * 105);
-       int icecream = (2 * 100);
-       int eggs = (4 * 70);
-       int weight = (bananas + milk + icecream + eggs);
-       int convert;
-        convert = (weight / 1000);
-        System.out.println("Общий вес продуктов: " + weight + "г" + "\n                        "
-               + convert + "кг");
+        int g = (1000 * 7);
+        int days1 = (g / 250);
+        int days2 = (g / 500);
+        int mean = ((days1 + days2) / 2);
+        System.out.println("Теряя 250г в день: " + days1 + " дней");
+        System.out.println("Теряя 500г в день: " + days2 + " дней");
+        System.out.println("В среднем " + mean + " дней");
     }
 }
